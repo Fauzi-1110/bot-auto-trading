205003977bc69e7babcff39d608fdba7b1811210
Fauzi-1110 <fauzinurwijaya1@gmail.com>
2026-03-23 22:39:00 +0700

    penambahaj fiyr kerja pada user

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require("./config.js")
 const botApi = require("node-telegram-bot-api")
 const token = global.token_bot
-const Client = new botApi(token, {polling: true})
+const Client = new botApi(token, {
+    polling: true
+})
 
 const c = require("chalk")
 const path = require("path")
@@ -11,33 +13,28 @@ const parser = require("uzi_telegram-parser")
 const fs = require("fs")
 
 const dbSystem = require("./lib/func_db.js")
-const { randomBotTrading,
+const {
+    randomBotTrading,
     updateBotName,
     addBotEffect,
-    clearBotEffect 
+    clearBotEffect
 } = require("./lib/func_bot.js")
-const { randItem,
+const {
+    randItem,
     randCoin
 } = require("./lib/func_cointem")
 
-if(!dbSystem)console.log("Error not find dbSystem file".red);
-<<<<<<< HEAD
-async function checkingGlobalDB(){
+if (!dbSystem)console.log("Error not find dbSystem file".red);
+async function checkingGlobalDB() {
     console.log(c.blueBright("\n📂 Checking global database..."))
     await dbSystem.createDBGlobal()
 }
 checkingGlobalDB()
-=======
-( async () => {
-    console.log(c.blueBright("\n📂 Checking global database..."))
-    await dbSystem.createDBGlobal()
-})()
->>>>>>> e0287652c3d978f1316f472fde1500d270f4abd1
 
 Client.on("message", async (m) => {
     try {
         require("./System/message.js")(Client, m)
-    } catch (e){
+    } catch (e) {
         console.log(e.stack.red)
     }
 })
@@ -45,69 +42,76 @@ Client.on("message", async (m) => {
 Client.on("callback_query", (q) => {
     try {
         require("./System/query.js")(Client, q)
-    } catch (e){
+    } catch (e) {
         console.log(e.stack.red)
     }
 })
 
-function backup(){
+function backup() {
     console.log(c.blueBright("\n🔁 Process backup database..."))
     Client.sendDocument("8141687459", path.join(process.cwd(), "database", "user.json"))
     Client.sendDocument("8141687459", path.join(process.cwd(), "database", "global.json"))
 }
 
-function resetProfitDaily(){
+function resetProfitDaily() {
     const allUser = Object.entries(dbSystem.getAllUser())
-    const listReset = ["perak", "emas", "platinum", "diamond", "balaceCoin", "roadaCoin", "flagCoin", "timenCoin", "loyaliCoin"]
-    
-    for(let [key, value] of allUser){
-        for(let i = 0; i < listReset.length; i++){
+    const listReset = ["perak",
+        "emas",
+        "platinum",
+        "diamond",
+        "balaceCoin",
+        "roadaCoin",
+        "flagCoin",
+        "timenCoin",
+        "loyaliCoin"]
+
+    for (let [key, value] of allUser) {
+        for (let i = 0; i < listReset.length; i++) {
             dbSystem.setUserKey(key, `profit.${listReset[i]}`, 0)
         }
         dbSystem.setUserKey(key, "total_profit", 0)
     }
 }
 
-<<<<<<< HEAD
-function pajakHarian(){
+function pajakHarian() {
     const all_user_data = dbSystem.getAllUser()
     const all_user_key = Object.keys(all_user_data)
-    
-    for(let key of all_user_key){
+
+    for (let key of all_user_key) {
         const user = all_user_data[key]
-        
-        if(user.total_profit < 0)continue
+
+        if (user.total_profit < 0)continue
         const pajak = Math.floor(user.total_profit * 0.05)
         dbSystem.addUserKey(key, "uang", -pajak)
     }
 }
 
-=======
->>>>>>> e0287652c3d978f1316f472fde1500d270f4abd1
-setInterval( async() => {
+setInterval(async() => {
     const now = moment().tz("Asia/Jakarta")
     const jam = now.format("HH")
     const menit = now.format("mm")
     const detik = now.format("ss")
-    
-    if(detik === "20")randomBotTrading()
-    if(detik === "00"){ randItem(); randCoin(); }
-    
+
+    if (detik === "20")randomBotTrading()
+    if (detik === "00") {
+        randItem(); randCoin();
+    }
+
     // waktu bot smarr
-    if(jam == "20" && menit == "01" && detik == "01"){ clearBotEffect("smart"); addBotEffect("smart", 5+dbSystem.getGlobalKey("season")) }
-    if(jam == "04" && menit == "01" && detik == "01") clearBotEffect("smart")
-    
+    if (jam == "20" && menit == "01" && detik == "01") {
+        clearBotEffect("smart"); addBotEffect("smart", 5+dbSystem.getGlobalKey("season"))
+    }
+    if (jam == "04" && menit == "01" && detik == "01") clearBotEffect("smart")
+
     // waktubbuatbbot fast
-    if(jam == "15" && menit == "01" && detik == "01") { clearBotEffect("fast"); addBotEffect("fast", 8) }
-    if(jam == "18" && menit == "01" && detik == "01") clearBotEffect("fast")
-    
-    if([menit, detik].every((k) => k === "00")) backup()
-<<<<<<< HEAD
-    if(jam === "23" && menit === "50" && detik === "00"){
+    if (jam == "15" && menit == "01" && detik == "01") {
+        clearBotEffect("fast"); addBotEffect("fast", 8)
+    }
+    if (jam == "18" && menit == "01" && detik == "01") clearBotEffect("fast")
+
+    if ([menit, detik].every((k) => k === "00")) backup()
+    if (jam === "23" && menit === "50" && detik === "00") {
         pajakHarian()
         resetProfitDaily()
     }
-=======
-    if(jam === "23" && menit === "50" && detik === "00") resetProfitDaily()
->>>>>>> e0287652c3d978f1316f472fde1500d270f4abd1
 }, 1000);
